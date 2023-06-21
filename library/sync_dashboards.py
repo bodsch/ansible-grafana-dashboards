c5c00575b56405749e39fed11066cd4adf4a9234
Bodo Schulz <bodo@boone-schulz.de>
2023-06-21 10:42:24 +0200

    add TailLogger

--- library/sync_dashboards.py
+++ library/sync_dashboards.py
@@ -7,11 +7,36 @@
 
 from __future__ import absolute_import, division, print_function
 import os
+import logging
+import collections
+import re
 import dirsync
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.bodsch.core.plugins.module_utils.directory import create_directory, current_state
-# from ansible_collections.bodsch.core.plugins.module_utils.lists import compare_two_lists
+
+
+class TailLogHandler(logging.Handler):
+
+    def __init__(self, log_queue):
+        logging.Handler.__init__(self)
+        self.log_queue = log_queue
+
+    def emit(self, record):
+        self.log_queue.append(self.format(record))
+
+
+class TailLogger(object):
+
+    def __init__(self, maxlen):
+        self._log_queue = collections.deque(maxlen=maxlen)
+        self._log_handler = TailLogHandler(self._log_queue)
+
+    def contents(self):
+        return '\n'.join(self._log_queue)
+
+    @property
+    def log_handler(self):
+        return self._log_handler
 
 
 class Sync(object):
@@ -25,9 +50,6 @@ class Sync(object):
 
         self.source_directory = module.params.get("source_directory")
         self.destination_directory = module.params.get("destination_directory")
-        self.owner = module.params.get("owner")
-        self.group = module.params.get("group")
-        self.mode = module.params.get("mode")
 
     def run(self):
         """
@@ -38,15 +60,52 @@ class Sync(object):
             msg="initial"
         )
 
-        include_pattern = ('^.*\.json$',)
+        tail = TailLogger(2)
+
+        logger = logging.getLogger('dirsync')
+        formatter = logging.Formatter('%(message)s')
+
+        log_handler = tail.log_handler
+        log_handler.setFormatter(formatter)
+        logger.addHandler(log_handler)
+        logger.setLevel(logging.DEBUG)
+
+        include_pattern = ('^.*\\.json$',)
 
         if os.path.isdir(self.source_directory):
-            args = {'create': 'False', 'verbose': 'False', 'purge': 'True', 'include': include_pattern}
+
+            args = {
+                'create': 'False',
+                'verbose': 'False',
+                'purge': 'True',
+                'include': include_pattern,
+                'logger': logger,
+            }
+
             dirsync.sync(self.source_directory, self.destination_directory, 'sync', **args)
 
-            self.module.log(msg=f"= state: {self.module.stdout}")
+            result['failed'] = False
+            result['msg'] = "The directories were successfully synchronised."
 
-        # dirsync.sync(self.source_directory, self.destination_directory, 'diff', logger=self.logger)
+        log_contents = tail.contents()
+
+        # self.module.log(msg=f"len: {len(log_contents)}")
+        # self.module.log(msg=f"log: {log_contents}")
+
+        if len(log_contents) > 0:
+            pattern = re.compile(r"(?P<directories_parsed>\d+).*directories parsed, (?P<files_copied>\d+) files copied")
+
+            re_result = re.search(pattern, log_contents)
+
+            files_copied = re_result.group('files_copied')
+
+            if files_copied:
+                if int(files_copied) == 0:
+                    result['changed'] = False
+                    result['msg'] = "The directories are synchronous."
+                elif int(files_copied) > 0:
+                    result['changed'] = True
+                    result['msg'] = "The directories were successfully synchronised."
 
         return result
 
@@ -65,16 +124,6 @@ def main():
         destination_directory=dict(
             required=True,
             type='str'
-        ),
-        owner=dict(
-            required=False
-        ),
-        group=dict(
-            required=False
-        ),
-        mode=dict(
-            required=False,
-            type="str"
         ),
     )
 
